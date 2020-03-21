02014626e9690e5fc73cd024d4afb9151f698d26
Johnnatan Messias <johnme@mpi-sws.org>
2020-03-21 19:00:31 +0100

    Added distribution of the number of cases since the first reported case for USA

--- utils/js/chart.js/utils.js
+++ utils/js/chart.js/utils.js
@@ -7,7 +7,8 @@ window.chartColors = {
 	green: 'rgb(75, 192, 192)',
 	blue: 'rgb(54, 162, 235)',
 	purple: 'rgb(153, 102, 255)',
-	grey: 'rgb(201, 203, 207)'
+	grey: 'rgb(201, 203, 207)',
+	steel_blue: 'rgb(70,130,180)'
 };
 
 (function(global) {
